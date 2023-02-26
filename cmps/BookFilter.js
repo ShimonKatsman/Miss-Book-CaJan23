cc4f18212b602d6f15b94c7dcf82b48c8dcc80f2
Shimon Katsman <shkatsman@gmail.com>
2023-02-26 23:59:02 +0200

    BookFilter work start

--- cmps/BookFilter.js
+++ cmps/BookFilter.js
@@ -3,20 +3,27 @@
 export default {
     template: `
         <section class="book-filter">
-            <input 
-                v-model="filterBy.title"
-                @input="filter" 
-                placeholder="Search"
-                type="text" />
+            <fieldset>
+            <legend>Book title</legend>
+                <input v-model="filterBy.title" @input="filter" 
+                placeholder="Search" type="text"/>
+            </fieldset> 
+
+            <fieldset>
+                <legend>Book on sale</legend>
+                <input @input="filter" type="radio" value="true" v-model="filterBy.picked"> true
+                <input type="radio" value="false" v-model="filterBy.picked"> false
+            </fieldset>
+
         </section>
     `,
     data() {
         return {
-            filterBy: { title: '', price: 0 },
+            filterBy: { title: '', price: 0, picked: '', },
         }
     },
     methods: {
-        filter(){
+        filter() {
             this.$emit('filter', this.filterBy)
         }
     }
