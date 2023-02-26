1e653b1fba3a690421b41ae33a529406e3f944ee
Shimon Katsman <shkatsman@gmail.com>
2023-02-26 23:42:09 +0200

    improved BookDetails

--- cmps/BookDetails.js
+++ cmps/BookDetails.js
@@ -1,18 +1,45 @@
 // בס"ד
 
+import LongTxt from './LongTxt.js'
+
 export default {
     props: ['book'],
     template: `
         <section class="book-details">
             <h2>{{ book.title }}</h2>
-            <h3>Price: {{ book.listPrice.amount }}</h3>
+            <h3>Price: §{{ book.listPrice.amount }}</h3>
+            <p>{{ pageMsg }}</p>
+            <p>{{ publishedStatus }}</p>
+            <em>{{ saleMsg }}</em>
             <img :src="book.thumbnail">
+            <LongTxt :length="10" :txt="book.description"/>
             <button @click="closeDetails">Close</button>
         </section>
+
     `,
     methods: {
-        closeDetails(){
+        closeDetails() {
             this.$emit('hide-details')
-        }
+        },
+    },
+    computed: {
+        pageMsg() {
+            if (this.book.pageCount > 500) return 'Serious Reading'
+            if (this.book.pageCount > 200) return 'Descent Reading'
+            if (this.book.pageCount > 100) return 'Just Reading'
+            if (this.book.pageCount < 100) return 'Light Reading'
+        },
+        publishedStatus() {
+            let date = new Date()
+            if (this.book.publishedDate < date - 10) return 'Vintage'
+            if (this.book.publishedDate < date - 1) return 'Shelf'
+            if (this.book.publishedDate > date - 1) return 'New'
+        },
+        saleMsg() {
+            return this.book.listPrice.isOnSale ? 'On Sale' : ''
+        },
+    },
+    components: {
+        LongTxt,
     }
 }
---
+++ cmps/LongTxt.js
@@ -0,0 +1,38 @@
+// בס"ד
+
+export default {
+    props: ['txt', 'length'],
+    template: `
+        <p>{{ toggleSummery }}</p>
+        <button @click="toggleBtn" >read <span>{{ btnTxt }}</span></button>
+`,
+
+    data() {
+        return {
+            btnTxt: 'more',
+        }
+    },
+    methods: {
+        toggleBtn() {
+            this.btnTxt = this.btnTxt === 'more' ? 'less' : 'more'
+        }
+    },
+    computed: {
+        toggleSummery() {
+            let summery = this.txt
+
+            if (this.btnTxt === 'more') {
+                return summery.split(' ').splice(0, +(this.length) - 1).join(' ')
+            }
+
+            return summery
+        },
+    },
+    created() {
+
+    },
+    components: {
+
+    },
+    emits: [],
+}
